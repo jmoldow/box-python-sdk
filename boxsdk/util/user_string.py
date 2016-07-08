d9fde5c1faa8833a8504e8478369425fc96e4c89
Jordan Moldow <jmoldow@box.com>
2016-07-08 00:56:51 -0700

    m

--- boxsdk/util/user_string.py
+++ boxsdk/util/user_string.py
@@ -2,42 +2,60 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from six import text_type, PY2
+from functools import wraps
+
+from six import binary_type, text_type, PY2
 from six.moves import UserString as PythonUserString
 
 
-__all__ = [str(b'UserString')]
+__all__ = [str('UserString')]
 
 
-if PY2:
+def _cast_argument_to_text(byte_string_type_error):
+    def decorator(method):
 
-    # Acts more like a "UserText" / "UserUnicode", instead of a "UserBytes".
-    class UserString(PythonUserString):
-        __doc__ = PythonUserString.__doc__
-        __slots__ = ()
+        @wraps(method)
+        def wrapper(self, text_like_object):
+            if isinstance(text_like_object, PythonUserString):
+                text_like_object = text_like_object.data
+            if isinstance(text_like_object, binary_type):
+                if PY2:
+                    # Since Python 2 often considers `str` to be a text type,
+                    # treat ascii-decodable `str` objects as text strings
+                    # instead of byte strings.
+                    try:
+                        text_like_object = text_like_object.decode('ascii')
+                    except UnicodeDecodeError:
+                        raise type_error
+                else:
+                    raise type_error
+            text = text_type(text_like_object)
+            return method(self, text)
 
-        ## Methods that need to be modified to operate with text / unicode, instead of bytes.
+        return wrapper
 
-        def __init__(self, seq):
-            super(UserString, self).__init__(self._to_text(seq))
+    return decorator
 
-        def __unicode__(self): return unicode(self.data)
 
-        def __add__(self, other): return super(UserString, self).__add__(self._to_text(other))
+_concat_type_error = TypeError(str("can't concat byte strings to text strings"))
+_cast_concat_argument_to_text = _cast_argument_to_text(_concat_type_error)
 
-        def __radd__(self, other): return super(UserString, self).__radd__(self._to_text(other))
 
-        ## Helper
+# Acts more like a "UserText" / "UserUnicode", instead of a "UserBytes".
+@wraps(PythonUserString, assigned=('__doc__',), updated=())
+class UserString(PythonUserString):
+    __slots__ = ()
 
-        def _to_text(self, obj):
-            """Convert object to a text/unicode-like object.
+    ## Methods that need to be modified to operate with text / unicode, instead of bytes.
 
-            :type obj: `object`
-            :rtype: :class:`UserString` or `unicode`
-            """
-            if isinstance(obj, (text_type, UserString)):
-                return obj
-            return text_type(obj)
+    @_cast_argument_to_text(TypeError(str("argument can't be byte string")))
+    def __init__(self, seq):
+        super(UserString, self).__init__(seq)
 
-else:
-    UserString = PythonUserString
+    @_cast_concat_argument_to_text
+    def __add__(self, other): return super(UserString, self).__add__(other)
+
+    @_cast_concat_argument_to_text
+    def __radd__(self, other): return super(UserString, self).__radd__(other)
+
+    def __unicode__(self): return text_type(self.data)
--- test/unit/util/test_user_string.py
+++ test/unit/util/test_user_string.py
@@ -9,7 +9,18 @@ from six.moves import UserString as PythonUserString
 from boxsdk.util.user_string import UserString
 
 
-TEXT_LIKE_OBJECTS = set([u'foo', u'ƒøø', str(b'foo'), PythonUserString(b'foo')])
+TEXT_LIKE_OBJECTS = [u'foo', u'ƒøø', PythonUserString(u'foo')]
+if PY2:
+    TEXT_LIKE_OBJECTS.extend([b'foo', PythonUserString(b'foo')])
+else:
+    TEXT_LIKE_OBJECTS.append(PythonUserString(u'ƒøø'))  # This is broken on PY2 (which is why we created our UserString subclass).
+
+
+BYTE_STRING_LIKE_OBJECTS = [u'ƒøø'.encode('utf-8')]
+if PY2:
+    BYTE_STRING_LIKE_OBJECTS.append(PythonUserString(u'ƒøø'.encode('utf-8')))
+else:
+    BYTE_STRING_LIKE_OBJECTS.append(b'foo')
 
 
 @pytest.fixture(params=TEXT_LIKE_OBJECTS)
@@ -34,8 +45,18 @@ def other_text_like_object(request, other_should_be_user_string):
     return request.param
 
 
+@pytest.fixture(params=BYTE_STRING_LIKE_OBJECTS)
+def byte_string_like_object(request):
+    return request.param
+
+
 def test_init(user_string):
     pass
+
+
+def test_init_rejects_byte_strings(byte_string_like_object):
+    with pytest.raises(TypeError):
+        UserString(byte_string_like_object)
 
 
 def test_str(user_string, text_like_object):
@@ -47,8 +68,22 @@ def test_repr(user_string):
 
 
 def test_add(user_string, text_like_object, other_text_like_object):
-    assert user_string + other_text_like_object == text_like_object + other_text_like_object
+    concat = user_string + other_text_like_object
+    assert isinstance(concat, UserString)
+    assert concat == text_like_object + other_text_like_object
+
+
+def test_add_rejects_byte_strings(user_string, byte_string_like_object):
+    with pytest.raises(TypeError):
+        user_string + byte_string_like_object
 
 
 def test_radd(user_string, text_like_object, other_text_like_object):
-    assert other_text_like_object + user_string == other_text_like_object + text_like_object
+    concat = other_text_like_object + user_string
+    assert isinstance(concat, UserString)
+    assert concat == other_text_like_object + text_like_object
+
+
+def test_radd_rejects_byte_strings(user_string, byte_string_like_object):
+    with pytest.raises(TypeError):
+        byte_string_like_object + user_string
